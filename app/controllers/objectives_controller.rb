c98d1103dd104a0b34801617675492ae2832cc69
Emile Ré <emile@weroom.com>
2016-07-11 20:37:19 +0200

    filter by team and level

--- app/controllers/objectives_controller.rb
+++ app/controllers/objectives_controller.rb
@@ -1,2 +1,19 @@
 class ObjectivesController < ApplicationController
+  before_action :root_objectives, only: :index
+
+  def index
+    render locals: {
+      level: search_params[:level]
+    }
+  end
+
+  private
+
+  def search_params
+    params.permit(:team_id, :level)
+  end
+
+  def root_objectives
+    @root_objectives = search_params[:team_id].present? ? Team.find(search_params[:team_id]).root_objectives : Objective.roots
+  end
 end
--- app/models/objective.rb
+++ app/models/objective.rb
@@ -2,4 +2,6 @@ class Objective < ActiveRecord::Base
   # Store objectives as a nested set in the DB
   acts_as_nested_set depth_column: :level, touch: true
   belongs_to :owner, class_name: User
+
+  scope :of_level, ->(level = nil) { level.blank? ? all : where(Objective.arel_table[:level].eq(level)) }
 end
