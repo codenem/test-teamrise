2926803cee95d09484558cd99a7ba6b2b58007e7
Emile Ré <emile@weroom.com>
2016-07-12 11:53:31 +0200

    add children_progress

--- app/models/objective.rb
+++ app/models/objective.rb
@@ -4,4 +4,18 @@ class Objective < ActiveRecord::Base
   belongs_to :owner, class_name: User
 
   scope :of_level, ->(level = nil) { level.blank? ? all : where(Objective.arel_table[:level].eq(level)) }
+
+
+  def children_progress
+    return if self.leaf?
+
+    sum = self.children.select(:progress_start, :progress_value, :progress_target).inject(0.0) do |sum, child|
+      sum + child.progress
+    end
+    sum / self.children.size
+  end
+
+  def progress
+    (progress_value - progress_start).to_f / (progress_target - progress_start)
+  end
 end
